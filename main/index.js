9c59254b78550adf06de08e109008e5153923774
Aziz Ali <aziz@ilovecoding.org>
2018-04-04 16:33:02 -0500

    react lifecycle when state changes

---
+++ main/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            text: 'initial text'
+        }
+        console.log('constructor');
+    }
+
+    handleChange(e){
+        this.setState({
+            text: e.target.value
+        })
+    }
+    shouldComponentUpdate(nextProp, nextState){
+        console.log('shouldComponentUpdate');
+        if(nextState.text === 'no'){
+            return false
+        }else{
+            return true
+        }
+    }
+    componentWillUpdate(){
+        console.log('componentWillUpdate');
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate');
+    }
+    componentWillMount(){
+        console.log('componentWillMount');
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+
+    render(){
+        console.log('render');
+
+        return (
+            <div>
+                {this.state.text} <br />
+                <input type="text"
+                    onChange={this.handleChange.bind(this)}
+                    value={this.state.text} />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
